62f8a04ef6f7f4dc4562d97f98f81eae77f4e02f
Samuel Li <hcsamuel.li@gmail.com>
2024-04-24 18:10:34 -0700

    fixed rayleigh energy scaling

--- src/mimopy/channels/rayleigh.py
+++ src/mimopy/channels/rayleigh.py
@@ -18,9 +18,10 @@ class Rayleigh(Channel):
         """Realize the channel. Energy is set by adjusting the expectation of the channel"""
         shape = (self.rx.N, self.tx.N)
         if seed is not None:
-            np.random.seed(self.seed)
+            np.random.seed(seed)
         if energy is None:
             energy = self.tx.N * self.rx.N
+        energy /= self.tx.N * self.rx.N
         self.channel_matrix = np.sqrt(energy / 2) * (
             np.random.randn(*shape) + 1j * np.random.randn(*shape)
         )
