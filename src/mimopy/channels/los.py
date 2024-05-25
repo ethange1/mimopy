fixed some bugs in channel energy normalization; added move_nodes and realize() on network level

src/mimopy/channels/los.py
@@ -19,7 +19,10 @@ class LoS(Channel):
 
     @property
     def aoa(self):
-        return get_relative_position(self.tx.array_center, self.rx.array_center)
+        range, az, el = get_relative_position(
+            self.tx.array_center, self.rx.array_center
+        )
+        return az, el
 
     @aoa.setter
     def aoa(self, _):
@@ -36,10 +39,12 @@ class LoS(Channel):
             AoA/AoD. If not specified, the angles are
             calculated based on the relative position of the transmitter and receiver.
         """
-        az, el = get_relative_position(self.tx.array_center, self.rx.array_center)
+        range, az, el = get_relative_position(
+            self.tx.array_center, self.rx.array_center
+        )
         tx_response = self.tx.get_array_response(az, el)
         rx_response = self.rx.get_array_response(az + np.pi, el + np.pi)
         # H = np.outer(tx_response, rx_response).T
         self.H = np.outer(rx_response, tx_response.conj())
-        self.normalize_energy(self.energy)
+        self.normalize_energy(self._energy)
         return self

src/mimopy/channels/rayleigh.py
@@ -21,7 +21,7 @@ class Rayleigh(Channel):
     def realize(self):
         """Realize the channel. Energy is used to adjusting the expectation of the channel"""
         np.random.seed(self.seed)
-        energy = self.energy / self.tx.N / self.rx.N
+        energy = self._energy / self.tx.N / self.rx.N
         shape = (self.rx.N, self.tx.N)
         self.channel_matrix = np.sqrt(energy / 2) * (
             np.random.randn(*shape) + 1j * np.random.randn(*shape)

src/mimopy/channels/spherical_wave.py
@@ -22,5 +22,5 @@ class SphericalWave(Channel):
         # get relative phase shift
         phase_shift = 2 * np.pi * d
         self.channel_matrix = np.exp(1j * phase_shift).T.conj()
-        self.normalize_energy(self.energy)
+        self.normalize_energy(self._energy)
         return self